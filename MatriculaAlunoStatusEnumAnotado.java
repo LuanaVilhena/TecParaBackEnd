576230c79e18c5351b14999ecb76c767ec68d9b6
Luana Vilhena <129390680+LuanaVilhena@users.noreply.github.com>
2024-03-28 20:39:20 -0300

    Create MatriculaAlunoStatusEnumAnotado.java
    Código anotado para revisão.

---
+++ MatriculaAlunoStatusEnumAnotado.java
@@ -0,0 +1,15 @@
+package br.com.alunoonline.api;
+
+//enum é utilizado para representar conjuntos de valores que são mutuamente exclusivos e pré-definidos
+public enum MatriculaAlunoStatusEnum {
+    APROVADO
+
+    ,
+    REPROVADO
+
+    ,
+    TRANCADO
+
+    ,
+    MATRICULADO;
+}
