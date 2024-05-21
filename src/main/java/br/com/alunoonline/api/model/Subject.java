e072ebc6fa328f67a7926786995942ad11d22d3c
Luana Vilhena <129390680+LuanaVilhena@users.noreply.github.com>
2024-05-21 14:46:41 -0300

    Update and rename Disciplina.java to Subject.java

--- src/main/java/br/com/alunoonline/api/model/Subject.java
+++ src/main/java/br/com/alunoonline/api/model/Subject.java
@@ -3,6 +3,7 @@ package br.com.alunoonline.api.model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
@@ -10,18 +11,26 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+
 @Entity
-public class Disciplina implements Serializable {
+public class Subject implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
+    private Long id;
     private String name;
 
     @ManyToOne
     @JoinColumn(name = "professor_id")
     private Professor professor;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
