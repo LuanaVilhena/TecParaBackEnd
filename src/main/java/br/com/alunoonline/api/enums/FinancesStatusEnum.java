d6bb1ad6f2c580a23dcce8a0c87c45208a295b79
Luana Vilhena <129390680+LuanaVilhena@users.noreply.github.com>
2024-05-21 15:04:01 -0300

    Add files via upload

---
+++ src/main/java/br/com/alunoonline/api/enums/FinancesStatusEnum.java
@@ -0,0 +1,9 @@
+package br.com.alunoonline.api.enums;
+
+public enum FinancesStatusEnum {
+    EM_DIA,
+    EM_ATRASO,
+    TRANCADO,
+    CANCELADO;
+
+}
---
+++ src/main/java/br/com/alunoonline/api/enums/RegistrationStudentStatusEnum.java
@@ -0,0 +1,8 @@
+package br.com.alunoonline.api.enums;
+
+public enum RegistrationStudentStatusEnum {
+    APROVADO,
+    REPROVADO,
+    TRANCADO,
+    MATRICULADO;
+}
