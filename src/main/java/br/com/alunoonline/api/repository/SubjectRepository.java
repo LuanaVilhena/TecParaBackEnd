6e40ecc33b3ce8235265e688cebc47090a141261
Luana Vilhena <129390680+LuanaVilhena@users.noreply.github.com>
2024-05-21 14:51:53 -0300

    Update and rename DisciplinaRepository.java to SubjectRepository.java

--- src/main/java/br/com/alunoonline/api/repository/SubjectRepository.java
+++ src/main/java/br/com/alunoonline/api/repository/SubjectRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+  List<Subject> findByProfessorId(Long professorId);
 }
